46f1b218a73ba1cd65e9d2ac245d67d3dbef5f66
Andrei Stezenko <astezenko@markupus.tech>
2024-07-28 11:34:52 +0300

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'abfb6581baa30d42d8af');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ddc5fe7b7a525e295d5e');
